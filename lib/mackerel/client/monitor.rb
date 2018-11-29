ae250296bb5c321e6b9973a2014b21dd93ce2787
r_takaishi <ryo.takaishi.0@gmail.com>
2018-11-29 18:22:46 +0900

    support to get monitor by id

--- lib/mackerel/client/monitor.rb
+++ lib/mackerel/client/monitor.rb
@@ -5,6 +5,11 @@ module Mackerel
         response = get 'monitors', options
         response.body.monitors
       end
+
+      def monitor(id)
+        response = get "monitors/#{id}"
+        response.body.monitor
+      end
     end
   end
 end
